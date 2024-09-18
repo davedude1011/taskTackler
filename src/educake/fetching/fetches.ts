6144d41224090f1f8bbfe2e3fb35a119ca9e7cdb
davedude1011 <tom2smallwood@gmail.com>
2024-09-18 22:57:41 +0100

    changed localhost fetch pointer to production site backend

--- src/educake/fetching/fetches.ts
+++ src/educake/fetching/fetches.ts
@@ -64,7 +64,7 @@ export async function loopAnswerQuestions() {
 }
 
 async function getGeminiAnswer(questionData: Question): Promise<string|null> {
-    const answerData = await fetch('http://localhost:3000/api/educake/getAnswer', {
+    const answerData = await fetch('https://task-tackler-nu.vercel.app/api/educake/getAnswer', {
         method: 'POST',
         headers: {
           'Content-Type': 'text/plain',
